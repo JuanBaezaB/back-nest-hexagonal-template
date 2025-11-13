9810947ce83356bf90a9e0c1c11ad38e05248fb6
juan.baeza <ext_jbaeza@coordinador.cl>
2025-11-13 13:27:21 -0300

    [refactor]: Organize command and query handlers into separate index files for better module structure

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,25 +1,12 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './infrastructure/controllers/users.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserTypeOrmEntity } from './infrastructure/adapters/persistence/user.typeorm.entity';
-import { UserRepositoryPort } from './application/ports/out/user.repository.port';
-import { UserPersistenceAdapter } from './infrastructure/adapters/persistence/user.persistence.adapter';
 import { CqrsModule } from '@nestjs/cqrs'; // <-- 1. Importar CqrsModule
-
-// 2. Importar todos los Handlers
-import { CreateUserHandler } from './application/commands/handlers/create-user.handler';
-import { UpdateUserHandler } from './application/commands/handlers/update-user.handler';
-import { DeleteUserHandler } from './application/commands/handlers/delete-user.handler';
-import { GetAllUsersHandler } from './application/queries/handlers/get-all-users.handler';
-import { GetUserByIdHandler } from './application/queries/handlers/get-user-by-id.handler';
-
-// 3. Crear listas para mantener el módulo limpio
-export const CommandHandlers = [
-  CreateUserHandler,
-  UpdateUserHandler,
-  DeleteUserHandler,
-];
-export const QueryHandlers = [GetAllUsersHandler, GetUserByIdHandler];
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommandHandlers } from './application/commands/handlers';
+import { UserRepositoryPort } from './application/ports/out/user.repository.port';
+import { QueryHandlers } from './application/queries/handlers';
+import { UserPersistenceAdapter } from './infrastructure/adapters/persistence/user.persistence.adapter';
+import { UserTypeOrmEntity } from './infrastructure/adapters/persistence/user.typeorm.entity';
+import { UsersController } from './infrastructure/controllers/users.controller';
 
 @Module({
   imports: [CqrsModule, TypeOrmModule.forFeature([UserTypeOrmEntity])],
